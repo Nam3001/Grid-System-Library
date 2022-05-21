05fa88c9a5f8f47867513353da81d41ec1982467
Nam3001 <91010964+Nam3001@users.noreply.github.com>
2022-05-21 18:18:34 +0700

    Create main.cpp

---
+++ nntung/main.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+#include "../../_src/Log.h"
+#include "../../_src/Utils.h"
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	string s;
+	cin>>s;
+	int a;cin>>a;
+	string m=s;
+	int k=a;
+    string tong="Bai:Ma hoa van ban";
+    LOG_WT("%s\n",tong.c_str());
+    string ten="Ten: Nguyen Nhat Tung";
+    LOG_IT("%s\n",ten.c_str());
+    string msv="Msv: 21T1080051";
+    LOG_IT("%s\n",msv.c_str());
+    string nhap="Xau Q: "+m;
+    LOG_DT("%s\n",nhap.c_str());
+    string nhap1="Khoa k "+k;
+    LOG_DT("%s\n",nhap1.c_str());
+    string so="Sau S: ";
+	for(int i=a-1;i>=0;i--) cout<<s[i];
+	for(int i=s.size()-1;i>=a;i--) cout<<s[i]; 
+    LOG_WT("%s\n",so.c_str());
+Bạn đã gửi
+ok
+Duy
+ok a
+Duy
+Hồ Anh Duy
+đưa cái ni lên thôi a chứ ko cần win vs build.bat ấy a
+Bạn đã gửi
+của a tùng pk a
+Duy
+Hồ Anh Duy
+ời a
+Bạn đã gửi
+sai mk a
+Duy
+a copy ấy a
+Duy
+Tïng80703
+Duy
+Hồ Anh Duy
+kí hiệu đặc biệt
+Bạn đã gửi
+kêu a tùng đọc cái mã nớ e vs
+Duy
+Hồ Anh Duy
+em đang kêu a
+Bạn đã gửi
+a tùng bật bảo mạat 2 lớp mẹ r
+Duy
+Hồ Anh Duy
+a tùng off cmnr
+Bạn đã gửi
+th rứa a làm đi
+Bạn đã gửi
+e chỉ cho nhanh
+Duy
+ok a
+Duy
+Hồ Anh Duy
+em có tk a tùng đây
+Bạn đã gửi
+a vô github đi
+Bạn đã gửi
+ok
+Duy
+Hồ Anh Duy
+r a
+Bạn đã gửi
+a vô team12
+Duy
+Hồ Anh Duy
+r a
+Bạn đã gửi
+vô add file gì đó
+Duy
+Hồ Anh Duy
+Bạn đã gửi
+create
+Duy
+Hồ Anh Duy
+Bạn đã gửi
+đc ch
+Bạn đã gửi
+r
+Bạn đã gửi
+a nhâp tên file
+Bạn đã gửi
+xong /
+Bạn đã gửi
+là hn tạo thư mục
+Bạn đã gửi
+ddc ch a
+Duy
+Hồ Anh Duy
+ok a
+Bạn đã gửi
+vd tung/
+Duy
+Hồ Anh Duy
+rồi a
+Bạn đã gửi
+là hn ra thư mục tung
+Bạn đã gửi
+ok
+Bạn đã gửi
+a tạo tên file đi
+Bạn đã gửi
+kiemtra.cpp
+Duy
+Hồ Anh Duy
+Bạn đã gửi
+name your file ấy a
+Bạn đã gửi
+a nhập chi đó
+Bạn đã gửi
+.cpp
+Duy
+Duy
+Hồ Anh Duy
+xong cop cái kia vào à a
